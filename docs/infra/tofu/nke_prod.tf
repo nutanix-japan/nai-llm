3806ac9e25b52bb5b7b7b7d99eb61a4130a54af8
Lakshmi Balaramane <lakshmi.balaramane@nutanix.com>
2024-06-06 16:35:41 -0400

    fixing dkp link

--- docs/infra/tofu/nke_prod.tf
+++ docs/infra/tofu/nke_prod.tf
@@ -1,28 +1,3 @@
-terraform {
-  required_providers {
-    nutanix = {
-      source  = "nutanix/nutanix"
-      version = "1.9.5"
-    }
-  }
-}
-
-data "nutanix_cluster" "cluster" {
-  name = var.cluster_name
-}
-data "nutanix_subnet" "subnet" {
-  subnet_name = var.subnet_name
-}
-
-provider "nutanix" {
-  username     = var.user
-  password     = var.password
-  endpoint     = var.endpoint
-  insecure     = true
-  wait_timeout = 60
-}
-
-
 resource "nutanix_karbon_cluster" "example_cluster" {
   name       = "example_cluster"
   version    = "1.25.6-0"
